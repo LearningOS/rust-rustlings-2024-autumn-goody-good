43fc932950e4e092075c04b1faba2536fcd28918
rouridushi <3459236837@qq.com>
2024-10-11 09:38:13 +0800

    conversations finished

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let parts: Vec<&str> = s.split(',').collect();
+
+        let name = parts.get(0).map(|&s| s.to_string()).unwrap_or_default();
+        if name.is_empty() {
+            return Person::default();
+        }
+
+        match parts.get(1).and_then(|s| s.trim().parse().ok()) {
+            Some(age) => {
+                // 检查是否有多余的部分
+                if parts.len() > 2 {
+                    Person::default()
+                } else {
+                    Person { name, age }
+                }
+            },
+            None => Person::default(),
+        }
     }
 }
 
